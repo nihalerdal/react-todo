9ff8ac8fad85565cfcb4772a1f92b12da37c59d7
nihalerdal <nihalerdall@gmail.com>
2024-07-22 18:23:51 -0700

    Save todoList into the storage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import TodoList from "./TodoList"; //Import TodoList component
 import AddTodoForm from "./AddTodoForm"; //Import AddTodoForm component
 
 function App() {
-  const [todoList, setTodoList] = React.useState([]);
+  
+  const [todoList, setTodoList] = React.useState(
+    JSON.parse(localStorage.getItem("savedTodoList")) || []);
+
+  React.useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+  
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]); //spread operator to add newtodo to list
