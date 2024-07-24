b6b8ce2b47ff2b05eaadffdd63efd89314449700
nihalerdal <nihalerdall@gmail.com>
2024-07-23 18:31:55 -0700

    Refactor for generic custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import * as React from "react";
 import TodoList from "./TodoList"; //Import TodoList component
 import AddTodoForm from "./AddTodoForm"; //Import AddTodoForm component
 
-function useSemiPersistentState() {
-  const [todoList, setTodoList] = React.useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || []
+function useSemiPersistentState(key, initialState) {
+  const [value, setValue] = React.useState(
+    JSON.parse(localStorage.getItem(key)) || initialState
   );
 
   React.useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
 
-  return [todoList, setTodoList];
+  return [value, setValue];
 }
 
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
+  const [todoList, setTodoList] = useSemiPersistentState("savedTodoList", []);
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]); //spread operator to add new todo to list
