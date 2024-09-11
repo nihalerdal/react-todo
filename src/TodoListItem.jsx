8b7438b51a59670b8ec3a3ad2c9e3666777966dc
nihalerdal <nihalerdall@gmail.com>
2024-09-10 22:27:33 -0700

    Add icon instead of remove button

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,16 +1,20 @@
 import styles from "./TodoListItem.module.css"
+import { FaTrash } from "react-icons/fa";
 
 function TodoListItem({ title, onRemoveTodo, id }) {
   return (
     <li className={styles.listItem}>
       <span>{title}</span>
-      <button
-        className={styles.removeButton}
-        type="button"
-        onClick={() => onRemoveTodo(id)}
-      >
-        Remove
-      </button>
+      <span className={styles.iconWrapper}>
+        <FaTrash
+          className={styles.removeButton}
+          type="button"
+          onClick={() => onRemoveTodo(id)}
+        >
+          {" "}
+          Remove
+        </FaTrash>
+      </span>
     </li>
   );
 }
