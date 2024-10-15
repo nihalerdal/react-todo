fe0b51bb1ea5d674d5041c7b61e323c83a03f4c2
nihalerdal <nihalerdall@gmail.com>
2024-10-14 18:33:56 -0700

    Refactor Navigation Bar

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,39 +1,12 @@
-import { BrowserRouter, Routes, Route, NavLink, Navigate, } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, } from "react-router-dom";
 import { useState, useEffect} from "react";
 import TodoContainer from "../Components/Todo/TodoContainer/TodoContainer";
 import Home from "../Components/Home/Home";
 import PageNotFound from "../Components/PageNotFound/PageNotFound";
 import Footer from "../Components/Footer/Footer";
-import DarkLightTheme from "../Components/DarkLightTheme/DarkLightTheme";
+import Navigation from "../Components/NavigationBar/NavigationBar";
 import styles from "./App.module.css";
 
-function Navigation({theme, toggleTheme}) {
-  return (
-    <nav className={styles.navContainer}>
-      <NavLink
-        to="/home"
-        className={({ isActive }) =>
-          isActive
-            ? `${styles.navItem} ${styles.activeNavItem}`
-            : styles.navItem
-        }
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/todo"
-        className={({ isActive }) =>
-          isActive
-            ? `${styles.navItem} ${styles.activeNavItem}`
-            : styles.navItem
-        }
-      >
-        Todo List
-      </NavLink>
-      <DarkLightTheme theme={theme} toggleTheme={toggleTheme} />
-    </nav>
-  );
-}
 
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
---
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+import styles from "./NavigationBar.module.css";
+import DarkLightTheme from "../DarkLightTheme/DarkLightTheme";
+
+
+function Navigation({ theme, toggleTheme }) {
+  return (
+    <nav className={styles.navContainer}>
+      <NavLink
+        to="/home"
+        className={({ isActive }) =>
+          isActive
+            ? `${styles.navItem} ${styles.activeNavItem}`
+            : styles.navItem
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/todo"
+        className={({ isActive }) =>
+          isActive
+            ? `${styles.navItem} ${styles.activeNavItem}`
+            : styles.navItem
+        }
+      >
+        Todo List
+      </NavLink>
+      <DarkLightTheme theme={theme} toggleTheme={toggleTheme} />
+    </nav>
+  );
+}
+
+export default Navigation;
